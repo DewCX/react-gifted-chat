fd4f5daef85248206c47e00f8e1cf4f6a626c861
Cem Mandraci <cemmandraci@hotmail.com>
2022-10-17 14:19:27 +0300

    #Add firebase to the project

---
+++ firebase.js
@@ -0,0 +1,29 @@
+import * as firebase from 'firebase';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyALnsrSPa6R5UBrEnzY47E-pqbWuHmMsDE",
+  authDomain: "gifted-chat-42d11.firebaseapp.com",
+  projectId: "gifted-chat-42d11",
+  storageBucket: "gifted-chat-42d11.appspot.com",
+  messagingSenderId: "479108116035",
+  appId: "1:479108116035:web:568bb23c000e6da7426ef6"
+};
+
+// Initialize Firebase
+let app = initializeApp(firebaseConfig);
+
+if (firebase.apps.length === 0) {
+    app = firebase.initializeApp(firebaseConfig);
+}else{
+    app = firebase.app()
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
+
+export {db, auth};
